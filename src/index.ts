c8fe5edfa10c8218f06128ca4edc7555241f004e
Luca Campana <luca.campa.94@gmail.com>
2019-06-21 20:27:18 +0200

    Setup google trends

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('!!!');
+const googleTrends = require('google-trends-api');
+
+class Main {
+
+    public static async main(){
+        const dailyTrends = await this.getDailyTrends();
+        console.log(JSON.stringify(JSON.parse(dailyTrends), null, 4));
+    }
+
+    private static async getDailyTrends(){
+        const trends = await googleTrends.dailyTrends({ geo: 'IT' });
+        return trends;
+    }
+
+}
+
+Main.main();
